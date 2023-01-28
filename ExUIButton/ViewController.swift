e91d15a48539e7fde6a725aa5d5cfa8207c2c7e1
jake <palatable7@naver.com>
2023-01-28 19:41:12 +0900

    Implement

---
+++ ExUIButton/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  ExUIButton
+//
+//  Created by 김종권 on 2023/01/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    private let button: UIButton = {
+        let button = UIButton()
+        
+        // text
+        button.titleLabel?.font = .systemFont(ofSize: 16)
+        button.setTitle("타이틀", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.setTitleColor(.blue, for: .highlighted)
+        
+        // image
+        button.setImage(UIImage(systemName: "person.crop.circle"), for: .normal)
+        button.setImage(UIImage(systemName: "person.crop.circle.fill"), for: .highlighted)
+        
+        // inset
+        let intervalSpacing = 6.0
+        let halfIntervalSpacing = intervalSpacing / 2
+        button.contentEdgeInsets = .init(top: 0, left: halfIntervalSpacing, bottom: 0, right: halfIntervalSpacing)
+        button.imageEdgeInsets = .init(top: 0, left: -halfIntervalSpacing, bottom: 0, right: halfIntervalSpacing)
+        button.titleEdgeInsets = .init(top: 0, left: halfIntervalSpacing, bottom: 0, right: -halfIntervalSpacing)
+        
+        
+        
+        button.contentEdgeInsets = .init(top: 0, left: 100, bottom: 0, right: 0)
+        
+        button.backgroundColor = .lightGray.withAlphaComponent(0.3)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(button)
+        NSLayoutConstraint.activate([
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+        ])
+    }
+}
